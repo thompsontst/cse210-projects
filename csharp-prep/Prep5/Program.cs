df4c28153314e615ebb741dc09c2f995c473ec09
thompsontst <thompsonabove@gmail.com>
2024-04-29 11:33:39 +0100

    correction on week one prep. 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,26 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-          DisplayWelcomeMessage();
+          DisplayWelcome();
+          string userName = PromptUserName();
+          int userNum = PromptUserNumber();
 
-        string userName = PromptUserName();
-        int userNumber = PromptUserNumber();
+          int getSquare = SquareNumber(userNum);
 
-        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, getSquare);
 
-        DisplayResult(userName, squaredNumber);
     }
 
-    static void DisplayWelcomeMessage()
+    static void DisplayWelcome()
     {
-        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine("welcome to the program");
     }
 
     static string PromptUserName()
     {
-        Console.Write("Please enter your name: ");
+        Console.Write("Please enter your name: " );
         string name = Console.ReadLine();
-
         return name;
     }
 
@@ -31,18 +30,17 @@ class Program
     {
         Console.Write("Please enter your favorite number: ");
         int number = int.Parse(Console.ReadLine());
-
         return number;
     }
 
     static int SquareNumber(int number)
     {
-        int square = number * number;
-        return square;
+        int squaredNumbers = number * number;
+        return squaredNumbers;
     }
 
-    static void DisplayResult(string name, int square)
+    static void DisplayResult(string name, int number)
     {
-        Console.WriteLine($"{name}, the square of your number is {square}");
+        Console.WriteLine($"{name} the square of the number is {number}");
     }
 }
